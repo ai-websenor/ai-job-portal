f3f22d65073910cc0323500f3b779ff06b21c465
Prafull Singh Rao <prafulls.websenor@gmail.com>
2026-02-27 10:51:44 +0530

    Introduce avatar filtering by gender and search, and extend IAvatar properties with gender, isActive, and displayOrder.

--- apps/job-board-web/src/app/components/profile/AvatarSection.tsx
+++ apps/job-board-web/src/app/components/profile/AvatarSection.tsx
@@ -1,39 +1,63 @@
-"use client";
+'use client';
 
-import ENDPOINTS from "@/app/api/endpoints";
-import http from "@/app/api/http";
-import useGetProfile from "@/app/hooks/useGetProfile";
-import { IAvatar } from "@/app/types/types";
-import { Avatar, Card, CardBody, Tooltip } from "@heroui/react";
-import { useEffect, useState } from "react";
-import LoadingProgress from "../lib/LoadingProgress";
-import ConfirmationDialog from "../dialogs/ConfirmationDialog";
-import useUserStore from "@/app/store/useUserStore";
+import ENDPOINTS from '@/app/api/endpoints';
+import http from '@/app/api/http';
+import useGetProfile from '@/app/hooks/useGetProfile';
+import { IAvatar } from '@/app/types/types';
+import { Avatar, Card, CardBody, Input } from '@heroui/react';
+import { useEffect, useState } from 'react';
+import LoadingProgress from '../lib/LoadingProgress';
+import ConfirmationDialog from '../dialogs/ConfirmationDialog';
+import useUserStore from '@/app/store/useUserStore';
+import { Gender } from '@/app/types/enum';
+import CommonUtils from '@/app/utils/commonUtils';
+import clsx from 'clsx';
+import { IoIosSearch } from 'react-icons/io';
 
 const AvatarSection = () => {
   const { user } = useUserStore();
   const { getProfile } = useGetProfile();
   const [loading, setLoading] = useState(false);
+  const [gender, setGender] = useState(Gender.male);
   const [avatars, setAvatars] = useState<IAvatar[]>([]);
+  const [debounceTime, setDebounceTime] = useState<NodeJS.Timeout | null>(null);
+
   const [confirmModal, setConfirmModal] = useState({
     show: false,
-    avatarId: "",
+    avatarId: '',
   });
 
-  const getAvatars = async () => {
+  const handleSearch = (query: string) => {
+    if (debounceTime) {
+      clearTimeout(debounceTime);
+    }
+
+    setDebounceTime(
+      setTimeout(() => {
+        getAvatars(query?.trim());
+      }, 1500),
+    );
+  };
+
+  const getAvatars = async (search?: string) => {
     try {
-      const response = await http.get(ENDPOINTS.AVATARS.LIST);
+      setLoading(true);
+      const response = await http.get(ENDPOINTS.AVATARS.LIST, {
+        params: { gender, search },
+      });
       if (response?.data) {
         setAvatars(response?.data);
       }
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     getAvatars();
-  }, []);
+  }, [gender]);
 
   const handleChooseAvatar = async (avatarId: string) => {
     try {
@@ -50,44 +74,66 @@ const AvatarSection = () => {
   return (
     <Card className="w-full shadow-none border border-gray-100 bg-white">
       <CardBody className="p-4">
-        <h3 className="text-sm font-bold text-gray-900 mb-4">Choose Avatar</h3>
-        {loading ? (
-          <LoadingProgress />
-        ) : (
-          <div className="grid grid-cols-4 gap-4">
-            {avatars.map((avatar) => (
-              <Tooltip
-                key={avatar.id}
-                content={avatar.name}
-                placement="top"
-                closeDelay={0}
+        <h3 className="text-sm text-center font-bold text-gray-900 mb-3">Choose Avatar</h3>
+
+        <div className="flex flex-col gap-3 mb-4">
+          <div className="flex justify-center gap-1 items-center">
+            {Object.values(Gender).map((item) => (
+              <button
+                key={item}
+                onClick={() => setGender(item)}
+                className={clsx(
+                  'text-[10px] py-0.5 px-3 hover:bg-secondary rounded-lg text-primary',
+                  gender === item && 'bg-secondary border border-primary',
+                )}
               >
-                <div
-                  onClick={() =>
-                    setConfirmModal({
-                      show: true,
-                      avatarId: avatar.id,
-                    })
-                  }
-                  className="cursor-pointer group relative flex justify-center"
-                >
-                  <Avatar
-                    color="primary"
-                    src={avatar.imageUrl}
-                    isBordered={avatar.imageUrl === user?.profilePhoto}
-                    className="w-12 h-12 transition-all duration-300 group-hover:scale-110 group-active:scale-95"
-                  />
-                </div>
-              </Tooltip>
+                {CommonUtils.keyIntoTitle(item)}
+              </button>
             ))}
           </div>
+
+          <Input
+            size="sm"
+            placeholder="Search by name"
+            onChange={(ev) => handleSearch(ev.target.value)}
+            endContent={<IoIosSearch size={17} className="text-gray-400" />}
+          />
+        </div>
+
+        {loading ? (
+          <LoadingProgress />
+        ) : avatars?.length > 0 ? (
+          <div className="grid grid-cols-4 gap-4">
+            {avatars.map((avatar) => (
+              <div
+                key={avatar.id}
+                onClick={() =>
+                  setConfirmModal({
+                    show: true,
+                    avatarId: avatar.id,
+                  })
+                }
+                className="cursor-pointer group relative flex flex-col items-center justify-center"
+              >
+                <Avatar
+                  color="primary"
+                  src={avatar.imageUrl}
+                  isBordered={avatar.imageUrl === user?.profilePhoto}
+                  className="w-14 h-14 transition-all duration-300 group-hover:scale-110 group-active:scale-95"
+                />
+                <p className="text-xs font-medium">{avatar.name}</p>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p className="text-xs text-center text-default-400">No Avatar found</p>
         )}
       </CardBody>
 
       {confirmModal.show && (
         <ConfirmationDialog
           isOpen={confirmModal.show}
-          onClose={() => setConfirmModal({ show: false, avatarId: "" })}
+          onClose={() => setConfirmModal({ show: false, avatarId: '' })}
           onConfirm={() => handleChooseAvatar(confirmModal.avatarId)}
           title="Confirm Avatar"
           message="Are you sure you want to change your avatar?"
