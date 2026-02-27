d9947b54b27bc4627cb8e50ad6ed8ec866a08238
Prafull Singh Rao <prafulls.websenor@gmail.com>
2026-02-27 17:02:49 +0530

    feat: Implement the RescheduleInterviewDialog component with UI, state management, and API integration for rescheduling interviews.

--- apps/job-board-web/src/app/components/dialogs/RescheduleInterviewDialog.tsx
+++ apps/job-board-web/src/app/components/dialogs/RescheduleInterviewDialog.tsx
@@ -1,12 +1,77 @@
 import { DialogProps, IInterview } from '@/app/types/types';
+import { getLocalTimeZone, today } from '@internationalized/date';
+import {
+  addToast,
+  Button,
+  DatePicker,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+} from '@heroui/react';
+import { useState } from 'react';
+import http from '@/app/api/http';
+import ENDPOINTS from '@/app/api/endpoints';
+import { InterviewStatus } from '@/app/types/enum';
 
 interface Props extends DialogProps {
   interview: IInterview;
   refetch: () => void;
 }
 
-const RescheduleInterviewDialog = ({ isOpen, onClose, interview }: Props) => {
-  return <div>RescheduleInterviewDialog</div>;
+const RescheduleInterviewDialog = ({ isOpen, onClose, refetch, interview }: Props) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleReschedule = async () => {
+    try {
+      setLoading(true);
+      await http.put(ENDPOINTS.EMPLOYER.INTERVIEWS.UPDATE(interview.id), {
+        status: InterviewStatus.rescheduled,
+      });
+      onClose();
+      refetch();
+      addToast({
+        title: 'success',
+        color: 'success',
+        description: 'Interview rescheduled successfully',
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} size="md">
+      <ModalContent>
+        {(onClose) => (
+          <>
+            <ModalHeader className="flex flex-col gap-1">Reschedule Interview</ModalHeader>
+            <ModalBody>
+              <div className="flex flex-col gap-4">
+                <DatePicker
+                  label="Reschedule To"
+                  placeholderValue={today(getLocalTimeZone())}
+                  minValue={today(getLocalTimeZone())}
+                  labelPlacement="outside"
+                />
+              </div>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="danger" variant="light" onPress={onClose}>
+                Cancel
+              </Button>
+              <Button color="primary" isLoading={loading} onPress={handleReschedule}>
+                Reschedule
+              </Button>
+            </ModalFooter>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
+  );
 };
 
 export default RescheduleInterviewDialog;
