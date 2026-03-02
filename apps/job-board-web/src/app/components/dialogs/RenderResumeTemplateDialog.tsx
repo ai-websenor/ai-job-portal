a4174f2dfafc0d9d5765e49e2bd90c501c6adf1a
Prafull Singh Rao <prafulls.websenor@gmail.com>
2026-03-02 16:47:09 +0530

    refactor: update RenderResumeTemplateDialog to accept an `html` object, adjust modal presentation, and remove direct HTML rendering.

--- apps/job-board-web/src/app/components/dialogs/RenderResumeTemplateDialog.tsx
+++ apps/job-board-web/src/app/components/dialogs/RenderResumeTemplateDialog.tsx
@@ -2,19 +2,20 @@ import { DialogProps } from '@/app/types/types';
 import { Modal, ModalBody, ModalContent, ModalHeader } from '@heroui/react';
 
 interface Props extends DialogProps {
-  renderedHtml: string;
+  html: {
+    renderedHtml: string;
+    renderConfig: any;
+  };
 }
 
-const RenderResumeTemplateDialog = ({ isOpen, onClose, renderedHtml }: Props) => {
+const RenderResumeTemplateDialog = ({ isOpen, onClose, html }: Props) => {
   return (
-    <Modal isOpen={isOpen} onClose={onClose} size="md" scrollBehavior="inside">
+    <Modal isOpen={isOpen} onClose={onClose} size="5xl" scrollBehavior="inside">
       <ModalContent>
         {() => (
           <>
-            <ModalHeader className="flex flex-col gap-1">Reschedule Interview</ModalHeader>
-            <ModalBody>
-              <div dangerouslySetInnerHTML={{ __html: renderedHtml }} />
-            </ModalBody>
+            <ModalHeader className="flex flex-col gap-1">Preview</ModalHeader>
+            <ModalBody></ModalBody>
           </>
         )}
       </ModalContent>
