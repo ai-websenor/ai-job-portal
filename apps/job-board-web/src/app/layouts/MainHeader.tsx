b0362179ae43f87ef872776debaae899a803ff9a
Prafull Singh Rao <prafulls.websenor@gmail.com>
2026-03-04 16:31:42 +0530

    refactor: remove LanguageSwitch component and its import from MainHeader.

--- apps/job-board-web/src/app/layouts/MainHeader.tsx
+++ apps/job-board-web/src/app/layouts/MainHeader.tsx
@@ -15,7 +15,6 @@ import { useMemo } from 'react';
 import { Roles } from '../types/enum';
 import useUserStore from '../store/useUserStore';
 import Notifications from '../components/notifications/Notifications';
-import LanguageSwitch from '../components/lib/LanguageSwitch';
 
 const MainHeader = () => {
   const router = useRouter();
@@ -115,7 +114,6 @@ const MainHeader = () => {
           </>
         ) : (
           <div className="flex gap-1 items-center">
-            <LanguageSwitch />
             {token && <Notifications />}
             <Button onPress={toggleMainDrawer} variant="light" size="sm">
               <HiMenuAlt1 size={18} />
