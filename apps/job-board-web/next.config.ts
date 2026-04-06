a321588816b5cac00215f42f525a45afa1ed3d11
Prafull Singh Rao <prafulls.websenor@gmail.com>
2026-04-06 11:07:20 +0530

    chore: update next.config.ts to allow all remote hostnames for http and https protocols

--- apps/job-board-web/next.config.ts
+++ apps/job-board-web/next.config.ts
@@ -7,18 +7,11 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '/**',
+        hostname: '**',
       },
       {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'ai-job-portal-dev-uploads.s3.ap-south-1.amazonaws.com',
-        pathname: '/**',
+        protocol: 'http',
+        hostname: '**',
       },
     ],
   },
